11d3967770f11b19e6387583b5270f130278cbba
fova0304 <fova0304@gmail.com>
2026-04-13 03:31:17 +0900

    add: authentication api added to get tokens

--- routers/auth.py
+++ routers/auth.py
@@ -3,6 +3,9 @@ from schema import UserCreateRequest
 from models import User
 from connection import get_async_session
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+
 
 router = APIRouter()
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
@@ -14,7 +17,7 @@ async def create_user_api(
     user = User(
         hashed_password = bcrypt_context.hash(body.hashed_password),
         user_name = body.user_name,
-        email = body.user_name,
+        email = body.email,
         role = body.role,
         is_active = True
     )
@@ -22,3 +25,23 @@ async def create_user_api(
     await session.commit()
     await session.refresh(user)
     return user
+
+async def authentication_user(user_name, hashed_password, session):
+    statement = select(User).where(User.user_name == user_name) #user_name is unique 
+    result = await session.execute(statement)
+    user = result.scalars().first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(hashed_password,user.hashed_password):
+        return False
+    return True
+
+
+@router.post("/token")
+async def get_token_api(
+    body: OAuth2PasswordRequestForm= Depends(), session = Depends(get_async_session)
+):
+    user = await authentication_user(body.username, body.password, session)
+    if not user:
+        return False
+    return True
